065710ca93f273c7c60747da49ecffe96b0d0755
koffi <jeankoffi543@gmal.com>
2025-09-12 02:28:09 +0000

    NCARE-34: Setting up the environment

---
+++ color.js
@@ -0,0 +1,11 @@
+export default {
+  black: '#000000',
+  primaryDark: '#1E003D',
+  primary: '#03BABA',
+  gray: '#757575',
+  grayLight: '#A7A9B1',
+  backgroundLight: '#EFF6FF',
+  background: '#F3F3F3',
+  textPrimary: '#000000',
+  textSecondary: '#757575',
+};
